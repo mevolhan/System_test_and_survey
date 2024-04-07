3f7d82679a8ad744f40e6a4c89ac1b947fb01a6b
Ivan Poplayhin <poplayhin2002@gmail.com>
2024-04-07 23:51:14 +0500

    v10

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/create_test')
+@app.route('/create-test')
 def create_test():
-    return render_template('create_test.html')
+    return render_template('create-test.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
